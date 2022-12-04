16f54a25daa01d3e9bee12dd87fb092388e3dcdc
AlexanderPanshin <57474084+AlexanderPanshin@users.noreply.github.com>
2022-12-04 10:16:55 +0300

    Add files via upload
    Добавлен Русский язык

---
+++ languages/ru.php
@@ -0,0 +1,60 @@
+/**** language strings ****/
+
+/* galleries */
+$lang["menu_title"] = "галереи";
+/* select a gallery from the galleries menu */
+$lang["menu_guide"] = " выберите галерею из меню галерей";
+
+/* back to thumbnails */
+$lang["lightbox_back"] = " вернуться к миниатюрам";
+
+/* Alert */
+$lang["alert_title"] = "тревога";
+/* click to dismiss */
+$lang["alert_dismiss"] = " щелкните, чтобы отклонить";
+/* there are no images in this gallery */
+$lang["alert_no_images"] = " в этой галерее нет изображений";
+/* click on image to enlarge */
+$lang["alert_enlarge"] = " щелкните изображение, чтобы увеличить";
+/* gallery does not work on your browser */
+$lang["alert_browser_error"] = " К сожалению, эта галерея не работает в вашем браузере";
+
+/* finding galleries */
+$lang["loading_menu"] = "поиск галерей";
+/* looking for images */
+$lang["loading_thumbs"] = " поиск изображений";
+/* image */
+$lang["loading_image"] = "изображение";
+/* of */
+$lang["loading_of"] = "из";
+/* loading */
+$lang["loading_title"] = "загрузка";
+
+/* reload */
+$lang["gallery_reload"] = "перезагрузка";
+
+
+
+/* pause */
+$lang["slideshow_pause"] = "пауза";
+/* resume */
+$lang["slideshow_resume"] = "аннотация";
+/* slideshow */
+$lang["slideshow_name"] = " слайд-шоу";
+
+/* tools */
+$lang["tools_title"] = "инструменты";
+/* show */
+$lang["tools_show"] = "показ";
+/* hide */
+$lang["tools_hide"] = "скрыть";
+
+/* change slide every */
+$lang["slideshow_change_slide"] = " изменить каждый слайд";
+/* seconds */
+$lang["slideshow_seconds"] = "секунды";
+/* start slideshow */
+$lang["slideshow_button"] = "начать слайд-шоу";
+
+/* permalink */
+$lang["permalink"] = " постоянная ссылка";
